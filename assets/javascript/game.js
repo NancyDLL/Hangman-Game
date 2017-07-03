603642d9178250216ff9c76f32014a968f6dec21
NancyDLL <nano14@mac.com>
2017-07-03 08:43:33 -0500

    Drafted html, connected css, connected javascipt, started psuedocode.

---
+++ assets/javascript/game.js
@@ -0,0 +1,10 @@
+//1 - Check connection to html page
+console.log("Hangman script connected")
+
+//Psuedocode
+//Create an array of questions and answers (can pairs of strings be stored in same array?)
+//Display question to User
+//Display blanks = letters in answer (each letter has a position in an array?)
+//User selects a letter and letter is compared against the answer for the question
+//If correct display letter in correct position (will I need to store each in an array or can array relate to each other in some way?)
+//If incorrect display letter in the incorrectGuesses ID
