69591944cb2c4b91fe187a0a918fc73aa2a9d537
NancyDLL <nano14@mac.com>
2017-07-05 13:10:27 -0500

    Learned switch case statement and got it to work.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,50 +1,70 @@
 //1 - Check connection to html page
 console.log("Hangman script connected")
-//2 - Set initial variables
+//Psuedocode
+//Create an array of questions and answers and set initial variables
 var userGuess=null;
 var guessesLeft=9;
 var guessesMade=[];
 var wins=0;
 var losses=0;
 var questions = [
-	"First Question?",
-	"Second Question?",
-	"Third Question?"
+	"What is a piece of code associated with a class or object to perform a task?",
+	"What is a language mechanism for binding data with methods that operate on that data?",
+	"What is a sequence of program instructions that perform a specific task?"
 ];
 var answers = [
-	"abcd",
-	"abcdef",
-	"abcdefgh"
+	"method",
+	"object",
+	"function"
 ];
 var letterChoice=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-console.log("Question Array ", questions);
-console.log("Answer Array ", answers);
-console.log("Letter Choices ", letterChoice);
-
-//Psuedocode
-//Create an array of questions and answers
-//Random pick a question and display to User
+//Randomly generate a question and display to User
 document.getElementById("start").addEventListener("click", function() {
 	var displayQuestion=questions[Math.floor(Math.random()*questions.length)];
-	console.log("The picked question is ", displayQuestion);
+	//console.log("The picked question is ", displayQuestion);
 	document.querySelector('#question').innerHTML=displayQuestion;
 	//Pull corresponding answer from array
 	var findQindex=questions.indexOf(displayQuestion);
 	console.log("The index of the question is ", findQindex);
-	//can i use the value of index in an if else to pull the answer?
-	if (findQindex==0) {
-		console.log("The corresponding answer is ", answers[0]);
-		} //is there something like an if else to search through more then 2 posibilities?
+	switch(findQindex){
+		case 0:
+			var pickedAnswer=answers[0];
+			break;
+		case 1:
+			var pickedAnswer=answers[1];
+			break;
+		case 2:
+			var pickedAnswer=answers[2];
+			break;
+	}
+	console.log("The picked answer is ", pickedAnswer);
+	//For every letter of the answer display underscores (somehow length will be used)
 });
 
 
 
-//For every letter of the answer display underscores (somehow length will be used)
+
 //User selects a letter and letter is compared against the answer for the question
 //If correct display letter in correct position (will I need to store each in an array or can array relate to each other in some way?)
 //If incorrect display letter in the incorrectGuesses ID
 
-//Display a win or loss after each round.
+//Display a win or loss and reset guesses left after each round.
 document.querySelector('#winDisplay').innerHTML = wins;
 document.querySelector('#lossDisplay').innerHTML = losses;
+document.querySelector('#guessesLeft').innerHTML = guessesLeft;
+
+//UNUSED - but working if the other does not.
+	//can i use the value of index in an if else to pull the answer?
+	// if (findQindex==0) {
+	// 	console.log("The corresponding answer is ", answers[0]);
+	// 	var pickedAnswer=answers[0];
+	// 	} 
+	// 	else if (findQindex==1){
+	// 		console.log("The corresponding answer is ", answers[1]);
+	// 		var pickedAnswer=answers[1];
+	// 	}
+	// 	else if (findQindex==2){
+	// 		console.log("The corresponding answer is ", answers[2]);
+	// 		var pickedAnswer=answers[2];
+	// 	}
