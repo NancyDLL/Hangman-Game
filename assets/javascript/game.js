a83c909203da37a597fefd5bf5fa2cd3ab4833b8
NancyDLL <nano14@mac.com>
2017-07-10 09:52:46 -0500

    More attempt at the correct guesses.

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -5,8 +5,10 @@ console.log("Hangman script connected")
 var userGuess=null;
 var guessesLeft=9;
 var guessesMade=[];
+var correctLetters=[];
 var wins=0;
 var losses=0;
+var undScoreCount=0;
 var questions = [
 	"What is a piece of code associated with a class or object to perform a task?",
 	"What is a language mechanism for binding data with methods that operate on that data?",
@@ -19,7 +21,8 @@ var answers = [
 ];
 var letterChoice=["a", "b", "c", "d", "e", "f", "g", "h", "i", "j", "k", "l", "m", "n", "o", "p", "q", "r", "s", "t", "u", "v", "w", "x", "y", "z"];
 
-//Randomly generate a question and display to User
+//Randomly generate a question and display to User 
+//pressing any key is used for guessing - how to use both to start the game and then after game started to select a letter?
 document.getElementById("start").addEventListener("click", function() {
 	var displayQuestion=questions[Math.floor(Math.random()*questions.length)];
 	//console.log("The picked question is ", displayQuestion);
@@ -30,17 +33,22 @@ document.getElementById("start").addEventListener("click", function() {
 	switch(findQindex){
 		case 0:
 			var pickedAnswer=answers[0];
+				//build a function that compares the letter selection against the letters in the array item
+			correctLetters="m","e","t","h","o","d"
+				//is there a way to build an array out of the string of pickedAnswer? for loop?
 			break;
 		case 1:
 			var pickedAnswer=answers[1];
+			correctLetters="o","b","j","e","c","t"
 			break;
 		case 2:
 			var pickedAnswer=answers[2];
+			correctLetters="f","u","n","c","t","i","o","n"
 			break;
 	}
 	console.log("The picked answer is ", pickedAnswer);
 	//find the length of the array item 
-	var undScoreCount=pickedAnswer.length;
+	undScoreCount=pickedAnswer.length;
 	console.log("The number of letters are ", undScoreCount);
 	//generate that number of "_ "
 	
@@ -49,19 +57,26 @@ document.getElementById("start").addEventListener("click", function() {
 
 	//User selects a letter and letter is compared against the answer for the question
 	document.onkeyup = function(){
-		var userGuess = String.fromCharCode(event.keyCode).toLowerCase();
+		userGuess = String.fromCharCode(event.keyCode).toLowerCase();
 		console.log("The User guessed " + userGuess);
 		}
 		//and letter is compared against the answer for the question
-		//if (userGuess == pickedAnswer){
-
-		//}
+		if (userGuess == correctLetters) {
+	 	console.log("same");
+	} else {
+	 	console.log("different");
+	 	guessesMade++;
+	 	guessesLeft--;
+	 	//If incorrect display letter in the incorrectGuesses ID
+	 	document.querySelector('#incorrectGuesses').innerHTML += userGuess + " ";
+	 	console.log("The user has guessed a total of " + guessesMade + " times.");
+	 		if (guessesLeft == 0) {
+	 			losses++;
+	 			guessesLeft=9;
+ 			}
+ 		}
 //If correct display letter in correct position (will I need to store each in an array or can array relate to each other in some way?)
-//If incorrect display letter in the incorrectGuesses ID
 });
-
-//build a function that compares the letter selection against the letters in the array item
-
 
 
 //Display a win or loss and reset guesses left after each round.
